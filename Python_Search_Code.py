6000493b2cf84f8a8bc2b905511815e0a3f1c126
Muhammad-H-Kashif <has.kashif@yahoo.com>
2025-06-13 20:13:00 +0800

    initial commit

---
+++ Python_Search_Code.py
@@ -0,0 +1,26 @@
+#Python_Search_Code
+
+
+from flask import Flask, request, render_template
+import wikipedia
+import requests
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        search = request.form["search"]
+
+        result = wikipedia.summary(search, sentences=2)
+        return f"<h1>{result}</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
